61bcac2b5d94e71b5ce89ae39b9dfcaf944afe76
schqiushui <orochi9999@gmail.com>
2016-06-20 15:05:53 +0800

    swap: make each swap partition have one address_space
        When I use several fast SSD to do swap, swapper_space.tree_lock is
        heavily contended.  This makes each swap partition have one
        address_space to reduce the lock contention.  There is an array of
        address_space for swap.  The swap entry type is the index to the array.
    
        In my test with 3 SSD, this increases the swapout throughput 20%.
    
        [akpm@linux-foundation.org: revert unneeded change to  __add_to_swap_cache]
        Signed-off-by: Shaohua Li <shli@fusionio.com>
        Cc: Hugh Dickins <hughd@google.com>
        Acked-by: Rik van Riel <riel@redhat.com>
        Acked-by: Minchan Kim <minchan@kernel.org>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
        Change-Id: I8ad2d301866b2873562a46df2952cf562b2b5aab
    
        *Adjust for htc's sources.
    
        Signed-off-by: schqiushui <orochi9999@gmail.com>
    
    Signed-off-by: schqiushui <orochi9999@gmail.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -137,7 +137,7 @@ static int lmk_vmpressure_notifier(struct notifier_block *nb,
 	if (pressure >= 95) {
 		other_file = global_page_state(NR_FILE_PAGES) -
 			global_page_state(NR_SHMEM) -
-			total_swapcache_pages;
+			total_swapcache_pages();
 		other_free = global_page_state(NR_FREE_PAGES);
 
 		atomic_set(&shift_adj, 1);
@@ -150,7 +150,7 @@ static int lmk_vmpressure_notifier(struct notifier_block *nb,
 
 		other_file = global_page_state(NR_FILE_PAGES) -
 			global_page_state(NR_SHMEM) -
-			total_swapcache_pages;
+			total_swapcache_pages();
 
 		other_free = global_page_state(NR_FREE_PAGES);
 
@@ -397,12 +397,12 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 
 	other_free = global_page_state(NR_FREE_PAGES);
 
-	if (global_page_state(NR_SHMEM) + global_page_state(NR_MLOCK) + total_swapcache_pages <
+	if (global_page_state(NR_SHMEM) + global_page_state(NR_MLOCK) + total_swapcache_pages() <
 		global_page_state(NR_FILE_PAGES))
 		other_file = global_page_state(NR_FILE_PAGES) -
 						global_page_state(NR_SHMEM) -
 						global_page_state(NR_MLOCK) -
-						total_swapcache_pages;
+						total_swapcache_pages();
 	else
 		other_file = 0;
 
