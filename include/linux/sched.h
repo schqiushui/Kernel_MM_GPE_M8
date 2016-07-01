ad86b6f42647a440def0d09d585106c61b098b22
schqiushui <orochi9999@gmail.com>
2016-07-01 19:00:11 +0800

    Revert "sched/rt: Avoid updating RT entry timeout twice within one tick period"
    This reverts commit dd46b88405ff00979f3bdebb9ae26b64f08f4f05.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1257,7 +1257,6 @@ struct sched_entity {
 struct sched_rt_entity {
 	struct list_head run_list;
 	unsigned long timeout;
-	unsigned long watchdog_stamp;
 	unsigned int time_slice;
 	int nr_cpus_allowed;
 
